e66bc87f38814b1c9d3981f1f2cf21b81f50a748
Mohammed Dawood <mahmedtony73@gmail.com>
2024-10-27 02:47:29 +0400

    add active to nav

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,9 +1,12 @@
+"use client";
 import Link from "next/link";
 import logoImg from "@/public/images/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 function MainHeader() {
+  const path = usePathname();
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href='/'>
@@ -19,10 +22,22 @@ function MainHeader() {
       <nav className={classes.nav}>
         <ul>
           <li>
-            <Link href='/meals'>Browse Meals</Link>
+            <Link
+              href='/meals'
+              className={path.startsWith("/meals") ? classes.active : undefined}
+            >
+              Browse Meals
+            </Link>
           </li>
           <li>
-            <Link href='/community'>Foodies Community</Link>
+            <Link
+              href='/community'
+              className={
+                path.startsWith("/community") ? classes.active : undefined
+              }
+            >
+              Foodies Community
+            </Link>
           </li>
         </ul>
       </nav>
