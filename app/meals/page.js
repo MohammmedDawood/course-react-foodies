a9154a0268a69671e2c2a7505472a30d40b9865d
Mohammed Dawood <mahmedtony73@gmail.com>
2024-10-28 01:38:53 +0400

    add fetch data using next as a full stack

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,10 @@
 import MealsGrid from "@/components/meals/meals-grid";
 import classes from "./page.module.css";
 import Link from "next/link";
+import { getAllMeals } from "@/lib/meals";
 
-function MealsPage() {
+async function MealsPage() {
+  const meals = await getAllMeals();
   return (
     <>
       <header className={classes.header}>
@@ -19,7 +21,7 @@ function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
