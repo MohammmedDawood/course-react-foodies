a639fb95e0f540ffe3634a97a222d003e35c605c
Mohammed Dawood <mahmedtony73@gmail.com>
2024-10-28 00:58:16 +0400

    init meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,25 @@
+import classes from "./page.module.css";
+import Link from "next/link";
+
 function MealsPage() {
-  return <h1>MealsPage</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delecious meals, created{" "}
+          <span className={classes.higlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite meal and enjoy a delicious lunch or dinner at
+          home
+        </p>
+        <p className={classes.cta}>
+          <Link href='/meals/share'>Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
+    </>
+  );
 }
 
 export default MealsPage;
