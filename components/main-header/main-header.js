d63a6e339ab531c749e586c34a99d29ade645fec
Mohammed Dawood <mahmedtony73@gmail.com>
2024-10-28 00:37:53 +0400

    add use client component to the smallest component

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,12 +1,11 @@
-"use client";
 import Link from "next/link";
 import logoImg from "@/public/images/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import NavLink from "./nav-link";
 
 function MainHeader() {
-  const path = usePathname();
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href='/'>
@@ -22,22 +21,10 @@ function MainHeader() {
       <nav className={classes.nav}>
         <ul>
           <li>
-            <Link
-              href='/meals'
-              className={path.startsWith("/meals") ? classes.active : undefined}
-            >
-              Browse Meals
-            </Link>
+            <NavLink href='/meals'> Browse Meals</NavLink>
           </li>
           <li>
-            <Link
-              href='/community'
-              className={
-                path.startsWith("/community") ? classes.active : undefined
-              }
-            >
-              Foodies Community
-            </Link>
+            <NavLink href='/community'> Foodies Community</NavLink>
           </li>
         </ul>
       </nav>
