6209314b65d9c701c5a9bd0a18965d12666dad86
Mohammed Dawood <mahmedtony73@gmail.com>
2024-10-29 01:58:43 +0400

    add meal details page

--- app/meals/[mealSLug]/page.js
+++ app/meals/[mealSLug]/page.js
@@ -1,5 +1,34 @@
-function MealDetails({ params }) {
-  return <div>MealDetails {params.mealSlug}</div>;
+import Image from "next/image";
+import { getMealBySlug } from "@/lib/meals";
+import classes from "./page.module.css";
+
+async function MealDetailsPage({ params }) {
+  const meal = getMealBySlug(params.mealSlug);
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            By <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions.replace(/\n/g, "<br>"),
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
 
-export default MealDetails;
+export default MealDetailsPage;
